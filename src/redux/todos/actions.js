84a81aa75a4fd2c8750e3d0e40464ff625a81725
fayshal69 <foyshalbinamir@gmail.com>
2024-08-23 20:12:22 +0600

    add: todos actions

---
+++ src/redux/todos/actions.js
@@ -0,0 +1,43 @@
+import { ADDTODO, CLEARCOMPLETED, COMPLETEALL, DELETETODO, SELECTCOLOR, TOGGLECOMPLETE } from "./actionTypes"
+
+export const addtodo = (todoText) => {
+    return {
+        action: ADDTODO,
+        payload: todoText
+    }
+}
+
+export const deletetodo = (todoId) => {
+    return {
+        type: DELETETODO,
+        payload: todoId
+    }
+}
+
+export const togglecomplete = (todoId) => {
+    return {
+        type: TOGGLECOMPLETE,
+        payload: todoId
+    }
+}
+
+export const completeall = () => {
+    return {
+        type: COMPLETEALL
+    }
+}
+
+export const clearcompleted = () => {
+    return {
+        type: CLEARCOMPLETED
+    }
+}
+
+export const selectcolor = (todoId, color) => {
+    return {
+        type: SELECTCOLOR,
+        payload: {
+            todoId, color
+        }
+    }
+}
