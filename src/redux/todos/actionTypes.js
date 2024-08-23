fcc8158c9203488efcf1f32d1daefdee3c4d2a8b
fayshal69 <foyshalbinamir@gmail.com>
2024-08-23 20:07:03 +0600

    add: todos action types

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDTODO = "todos/addtodo";
+export const COMPLETEALL = "todos/completeall";
+export const CLEARCOMPLETED = "todos/clearcompleted";
+export const TOGGLECOMPLETE = "todos/togglecomplete";
+export const DELETETODO = "todos/deletetodo";
+export const SELECTCOLOR = "todos/selectcolor";
