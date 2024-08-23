137bd584c7f3d0e61594cd7a1fe4f3de36e12b01
fayshal69 <foyshalbinamir@gmail.com>
2024-08-23 16:52:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar"
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <div className="h-[90vh] w-full flex justify-center items-center bg-gradient-to-br from-sky-50 to-purple-100">
+        <div className="w-full max-w-[600px] bg-white p-10 mx-2 rounded-lg shadow-lg">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
