f0e05fa9d44c96e38f34c14c6ee5a5ab4dd54cc7
fayshal69 <foyshalbinamir@gmail.com>
2024-08-23 22:22:38 +0600

    add: filter options

--- src/components/FilterOptions.jsx
+++ src/components/FilterOptions.jsx
@@ -1,18 +1,42 @@
+import { useDispatch, useSelector } from "react-redux";
+import { filtercolor, filterstatus } from "../redux/filters/actions";
+
 const FilterOptions = () => {
+
+    const filters = useSelector((state) => state.filters); 
+    const dispatch = useDispatch();
+
+    const { status, colors } = filters;
+
+    const handleStatusChange = (status) => {
+        dispatch(filterstatus(status));
+    }
+
+    const handleColorChange = (color) => {
+        if(colors.includes(color)) {
+            dispatch(filtercolor(color, 'remove'));
+        } else {
+            dispatch(filtercolor(color, 'add'));
+        }
+        
+    }
+
     return (
         <div className="flex justify-between items-center gap-3">
             <p className="text-sm text-gray-600">Filter Options : </p>
-            <ul className="flex gap-1.5 items-center justify-center text-sm font-medium">
-                <li><button>All</button></li>
+            <ul className="flex gap-1.5 items-center justify-center text-sm">
+                <li className={`${status === 'All' && 'font-semibold'}`}><button onClick={() => handleStatusChange("All")}>All</button></li>
                 <li>|</li>
-                <li><button>Complete</button></li>
+                <li className={`${status === 'Complete' && 'font-semibold'}`}><button onClick={() => handleStatusChange("Complete")}>Complete</button></li>
                 <li>|</li>
-                <li><button>Uncomplete</button></li>
+                <li className={`${status === 'Incomplete' && 'font-semibold'}`}><button onClick={() => handleStatusChange("Incomplete")}>Incomplete</button></li>
                 <li>
                     <div className="flex justify-center items-center gap-1.5 ms-3">
-                        <button className='p-1.5 bg-red-500 hover:bg-red-500 rounded-full border-2 border-red-500'></button>
-                        <button className='p-1.5 bg-green-500 hover:bg-green-500 rounded-full border-2 border-green-500'></button>
-                        <button className='p-1.5 bg-yellow-500 hover:bg-yellow-500 rounded-full border-2 border-yellow-500'></button>
+                        <button onClick={() => handleColorChange("red")} className={`p-1.5 hover:bg-red-500 rounded-full border-2 border-red-500 ${colors.includes('red') && 'bg-red-500'}`}></button>
+
+                        <button onClick={() => handleColorChange("green")} className={`p-1.5 hover:bg-green-500 rounded-full border-2 border-green-500 ${colors.includes('green') && 'bg-green-500'}`}></button>
+
+                        <button onClick={() => handleColorChange("yellow")} className={`p-1.5 hover:bg-yellow-500 rounded-full border-2 border-yellow-500 ${colors.includes('yellow') && 'bg-yellow-500'}`}></button>
                     </div>
                 </li>
             </ul>
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { RiPlayListAddLine } from "react-icons/ri";
 import { TiTick } from "react-icons/ti";
-import { useDispatch } from "react-redux";
-import { addtodo } from "../redux/todos/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { addtodo, clearcompleted, completeall } from "../redux/todos/actions";
 
 const TodoInput = () => {
 
     const dispatch = useDispatch();
+    const todos = useSelector((state) => state.todos);
 
     const [text, setText] = useState('');
 
@@ -21,11 +22,19 @@ const TodoInput = () => {
         toast.success("Todo added successfully!");
     } 
 
+    const handleCompleteAll = () => {
+        dispatch(completeall(todos));
+    }
+
+    const handleCleatCompleted = () => {
+        dispatch(clearcompleted(todos));
+    }
+
     return (
         <>
             <form onSubmit={handleAddTodo}>
                 <div className="relative">
-                    <input value={text} onChange={(e) => setText(e.target.value)} type="text" className="w-full bg-gray-100 text-xl p-5 text-gray-600 focus:outline-none rounded-lg" placeholder="Write your todo..." />
+                    <input value={text} onChange={(e) => setText(e.target.value)} type="text" className="w-full bg-gray-100 text-xl p-5 pr-20 text-gray-600 focus:outline-none rounded-lg" placeholder="Write your todo..." />
 
                     <button type="submit" title="Add your todo" className="bg-emerald-300 absolute p-2 rounded-full text-2xl right-5 top-1/2 -translate-y-1/2 cursor-pointer duration-300 hover:bg-emerald-500 ">
                         <RiPlayListAddLine />
@@ -33,8 +42,8 @@ const TodoInput = () => {
                 </div>
             </form>
             <div className="flex items-center justify-between py-5 text-gray-600 font-light text-sm">
-                <button className="px-2 py-0.5 bg-gray-100 hover:bg-gray-200 duration-300 rounded-lg flex items-center gap-1">Complete All Tasks <TiTick className="text-lg text-emerald-600" /></button>
-                <button className="px-2 py-0.5 bg-gray-100 hover:bg-gray-200 duration-300 rounded-lg">Clear All Completed</button>
+                <button onClick={handleCompleteAll} className="px-2 py-0.5 bg-gray-100 hover:bg-gray-200 duration-300 rounded-lg flex items-center gap-1">Complete All Tasks <TiTick className="text-lg text-emerald-600" /></button>
+                <button onClick={handleCleatCompleted} className="px-2 py-0.5 bg-gray-100 hover:bg-gray-200 duration-300 rounded-lg">Clear All Completed</button>
             </div>
         </>
     );
